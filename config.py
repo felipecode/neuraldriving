0d2f25b4f52b1bcb9102d238a44e04e88478f29e
felipecode <felipe.alcm@gmail.com>
2016-08-01 14:50:04 +0200

    The first working version of the new arctecture

--- config.py
+++ config.py
@@ -25,18 +25,30 @@ class configTrain:
 		self.lambda_l2 = 1e-4
 		self.output_weigth = [1]
 		self.network_name = 'alexdrop'
+		self.restore = False
+
+		""" Add sannity Checks """
+
+		if self.restore not in (True, False):
+			raise Exception('Wrong restore option. (True or False)')
+
 
 class configOutput:
 
 
-	def __init__(self,models_path_main):
+	def __init__(self,models_path_main,batch_size_main,batch_size_val_main,number_images_epoch_main,number_images_epoch_val_main):
 
 		self.print_interval = 2
 		self.summary_writing_period = 20
 		self.validation_period = 200  # I consider validation as an output thing since it does not directly affects the training in general
 		self.feature_save_interval  = 100
+		
 		self.models_path =  models_path_main
 		self.variable_names = ['Steer']
+		self.number_images_epoch = number_images_epoch_main
+		self.number_images_epoch_val = number_images_epoch_val_main
+		self.batch_size = batch_size_main
+		self.batch_size_val = batch_size_val_main
 
 		""" Feature Visualization Part """
 		#self.histograms_list=[]
@@ -63,7 +75,6 @@ class configMain:
 						
 		self.input_size = (210,280,3)
 		self.output_size = (2)
-		self.restore = False
 		self.dropout = [1,1,1,1,1,1,0.5,0.5]
 		#self.dropout = [1,1,1,1,1,1,1,1]
 		
@@ -77,12 +88,8 @@ class configMain:
 
 		self.config_input = configInput(self.input_size,self.number_images_epoch,self.number_images_epoch_val,self.number_steering_bins)
 		self.config_train = configTrain(self.input_size,self.output_size,self.dropout)
-		self.config_output = configOutput(self.models_path)
+		self.config_output = configOutput(self.models_path,self.batch_size,self.batch_size_val,self.number_images_epoch,self.number_images_epoch_val)
 
 
 
-		""" Add sannity Checks """
-
-		if self.restore not in (True, False):
-			raise Exception('Wrong restore option. (True or False)')
 
--- main_train.py
+++ main_train.py
@@ -3,7 +3,7 @@ from input_data_driving import DataSetManager
 from config import *
 from input_class import *
 from train_class import *
-from output import *
+from output_class import *
 
 """Structure"""
 import sys
@@ -22,8 +22,7 @@ import numpy as np
 import os
 from optparse import OptionParser
 from PIL import Image
-import subprocess
-import time
+
 
 
 
@@ -40,35 +39,19 @@ manager = input_manager.get_dataset_manager()
 training_manager = Train(config.config_train)
 
 
-training_manager.build_network(config.config_train)
-training_manager.build_optimization(config.config_train)
+training_manager.build_network()
+training_manager.build_optimization()
 
 """ Initializing Session as variables that control the session """
 
 
-global_step = tf.Variable(0, trainable=False, name="global_step")
 
 training_manager.start_session()
 
-#sess = tf.InteractiveSession()
-#sess.run(tf.initialize_all_variables())
-#saver = tf.train.Saver(tf.all_variables())
-
-if config.restore == True
-
-"""Load a previous model if restore is set to True"""
 
 
-if config.restore:
-  if not os.path.exists(config.models_path):
-  os.mkdir(config.models_path)
-  ckpt = tf.train.get_checkpoint_state(config.models_path)
-  if ckpt:
-    print 'Restoring from ', ckpt.model_checkpoint_path  
-    saver.restore(sess,ckpt.model_checkpoint_path)
-else:
-  ckpt = 0
-
+"""Load a previous model if it is configured to restore """
+training_manager.restore_session()
 
 
 
@@ -78,51 +61,40 @@ else:
 
 """ Get the Last Iteration Trained """
 
-if ckpt:
-  initialIteration = int(ckpt.model_checkpoint_path.split('-')[1])
-else:
-  initialIteration = 1
+initialIteration = training_manager.get_last_iteration()
 
 
 
 
+output_manager = Output(config.config_output,input_manager,training_manager)
 
-feedDict = {dout:config.dropout}
-
-training_start_time =time.time()
-
-
-output_manager = Output(config.config_output,input_manager,training_manager,sess)
-
-for i in range(initialIteration, number_iterations):
+for i in range(initialIteration, config.number_iterations):
 
 
 
  
   """ Get the training batch """
-  batch = dataset.train.next_batch(config.batch_size)
+  batch = manager.train.next_batch(config.batch_size)
   
   """Save the model every 300 iterations"""
 
   if i%300 == 0:
-    saver.save(sess, config.models_path + 'model.ckpt', global_step=i)
-    print 'Model saved.'
 
-  
+    training_manager.save_model(i)
+
 
 
   start_time = time.time()
 
   """ Run the training step and monitor its execution time """
 
-  feedDict.update({x: batch[0], y_: batch[1]})
-
-  sess.run(training_manager.get_train_step(), feed_dict=feedDict)
+  training_manager.run_train_step(batch)
+  
 
   duration = time.time() - start_time
 
   """ With the current trained net, let the outputmanager print and save all the outputs """
-  output_manager.print_outputs(i) 
+  output_manager.print_outputs(i,duration,training_manager.get_feed_dict()) 
 
 
 
